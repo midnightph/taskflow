6a366ef6323189c27ca386c64591beab2fe09361
midnightph <pedrorodacinski26@gmail.com>
2026-03-26 05:59:20 -0300

    Handle validation errors in global exception handler

--- src/main/java/com/github/midnightph/taskflow/exception/GlobalExceptionHandler.java
+++ src/main/java/com/github/midnightph/taskflow/exception/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -24,6 +25,10 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.CONFLICT).body(error);
     }
 
-    
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        ErrorResponse error = new ErrorResponse(400, ex.getBindingResult().getFieldErrors().get(0).getDefaultMessage(), LocalDateTime.now());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
     
 }
